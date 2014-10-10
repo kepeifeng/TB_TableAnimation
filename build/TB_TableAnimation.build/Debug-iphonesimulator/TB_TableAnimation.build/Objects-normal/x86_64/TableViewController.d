71d2ae82cb8c2f71fdf6b06aad2fd41742f6b1a1
Kent Peifeng Ke <kepeifeng@gmail.com>
2014-10-10 16:56:29 +0800

    Delete build folder

--- build/TB_TableAnimation.build/Debug-iphonesimulator/TB_TableAnimation.build/Objects-normal/x86_64/TableViewController.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/apple/Documents/Projects/Open\ Source\ Project/TB_TableAnimation/TableTest2/TableViewController.m \
-  /Users/apple/Documents/Projects/Open\ Source\ Project/TB_TableAnimation/TableTest2/TableViewController.h
